e89195a2869343d58538af369fdc6424b29da765
Johnny Konczal <johndrostan@outlook.com>
2026-03-04 15:21:55 -0800

    feat: Display the application version in the page title and main heading by passing it to the template.

--- ui.py
+++ ui.py
@@ -1,8 +1,9 @@
 import flask
 from flask import *
+from version import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', version=v)
